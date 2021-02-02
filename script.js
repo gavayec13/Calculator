f4deefde5018df487bc9d4dc263fd603c1598559
gavayec13 <gavayec13@gmail.com>
2021-02-02 16:49:33 +0200

    'fixed'

--- script.js
+++ script.js
@@ -3,14 +3,22 @@ const input = document.getElementById('input');
 const insert = num => input.value += num;
 
 document.addEventListener('keydown', Event => {
-    if(Event.key.match(/[0-9%\/*\-+\.]/)) {
+    if(Event.key.match(/[0-9\/*\-+\.]/)) {
+        
         input.value += Event.key;
+        err();
     } else if(Event.key.match(/Backspace/)) {
         back();
     } else if(Event.key === 'Enter') {
         input.value = eval(input.value);
     }
-})
+});
+
+function err() {
+    let arr = ['+', '-', '/', '*'];
+    arr.map(i => input.value.endsWith(i) ? console.log('plus') : console.log('no'))
+    
+}
 
 const clean = () => input.value = '';
 
