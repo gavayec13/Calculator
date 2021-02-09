11342e975b36c5f69e44c4e407ed855f676f6945
gavayec13 <gavayec13@gmail.com>
2021-02-09 16:59:46 +0200

    fixed

--- script.js
+++ script.js
@@ -19,3 +19,12 @@ const back = () => input.value = input.value.substring(0, input.value.length-1);
 
 const equal = () => input.value ? input.value = eval(input.value) : null;
 
+
+if(!localStorage.theme) localStorage.theme = 'light'
+document.body.className = localStorage.theme
+darkModeBtn.onclick = () => {
+    console.log('click mode button');
+    document.body.classList.toggle('dark');
+    darkModeBtn.innerText = document.body.classList.contains('dark') ? 'DARK' : 'LIGHT';
+    localStorage.theme = document.body.className || 'light';
+}
